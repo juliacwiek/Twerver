bdcd3b3936377d9024add5dff32f0662d1921ee3
Cwiek Julia <cwiekjul@teach.cs.toronto.edu>
2020-02-09 17:36:08 -0500

    commiting file

--- a2/ftree.c
+++ a2/ftree.c
@@ -28,20 +28,35 @@ struct TreeNode *generate_ftree(const char *fname) {
     // of the FTree.  For files at other depths, the path would be the
     // file path from the root to that file.
 
-    // create new tree, root is the current file
+    // create new tree, where the root of the tree is the current file
     struct TreeNode *fileSystem = malloc(sizeof(struct TreeNode));
+
+    // check to see if malloc call failed
+    if (fileSystem == NULL) {
+    	fprintf(stderr, "Fatal: failed to allocate bytes");
+    	exit(1);
+    }
 
     // set name of the current file to this node's fname
     fileSystem->fname = malloc(sizeof(char) * strlen(fname) + 1);
+
+    // check to see if malloc call failed
+    if (fileSystem->fname == NULL) {
+    	fprintf(stderr, "Fatal: failed to allocate bytes");
+    	exit(1);
+    }
+
+    // make name of root of this tree be fname
     strcpy(fileSystem->fname, fname);
 
     // initialize struct stat and then read in info about the current file 
     // to this struct stat using lstat
     struct stat stat_buf;
 
+    // error check lstat
     if (lstat(fname, &stat_buf) == -1) {
-	        perror("lstat");        
-	        //return 1;    
+	    fprintf(stderr, "lstat error");       
+	    exit(1);    
     }
 
     // set permissions attribute for this node
@@ -53,11 +68,17 @@ struct TreeNode *generate_ftree(const char *fname) {
     fileSystem->contents = NULL;
     fileSystem->next = NULL;
 
+    // find type of file and set type attribute:
 
-    // find type of file and set as attribute
-    if (S_ISREG(stat_buf.st_mode)) { fileSystem->type = '-'; }
+    // regular file
+    if (S_ISREG(stat_buf.st_mode)) { 
+    	fileSystem->type = '-'; 
+    }
 
-    else if (S_ISLNK(stat_buf.st_mode)) { fileSystem->type = 'l'; }
+    // link
+    else if (S_ISLNK(stat_buf.st_mode)) { 
+    	fileSystem->type = 'l';
+    }
 
     // else, the file is a directory
     else {
@@ -66,47 +87,81 @@ struct TreeNode *generate_ftree(const char *fname) {
     	DIR *d_ptr = opendir(fname);
 
         if (d_ptr == NULL) {
-        	perror("opendir");
-        	//return 1;
+        	fprintf(stderr, "opendir error");
+        	exit(1);
         }
  
         // stores info about a directory entry
-        struct dirent *entry_ptr;
-
-        // call readdir to get struct dirent, which represents a directory entry
-        entry_ptr = readdir(d_ptr);
+        struct dirent *curr_file;
 
         // traverse directory, making recursive calls as you go
+
         int files_traversed = 1;
-        struct dirent *curr_file = entry_ptr;
-        while (curr_file != NULL) {
-        	if ((strcmp(curr_file->d_name,".") == 0) && (strcmp(curr_file->d_name, "..") == 0)) {
-        		struct TreeNode *curr_node = fileSystem->contents;
+        while ((curr_file = readdir(d_ptr)) != NULL) {
+        	
+        	// current file is not . or ..
+        	if ((strcmp(curr_file->d_name,".") != 0) && (strcmp(curr_file->d_name, "..") != 0)) {
+                
+                // if first file in the directory (will be contents attribute of the directory)
         		if (files_traversed == 1) {
-        			fileSystem->contents = generate_ftree(curr_file->d_name);
-        			curr_file = readdir(d_ptr);
+
+        			char path[1024];
+
+        			// if file is in the current directory, path is file name
+        			if (strcmp(fname, ".") == 0) { 
+        				strcpy(path, curr_file->d_name); 
+        				// set null pointer??
+
+        			// otherwise, path is in format abc/def/filename
+        			} else {
+        				strcpy(path, fname);
+        				strcat(path, "/");
+        				strcat(path, curr_file->d_name);
+        				// set null pointer???
+        			}
+
+        			fileSystem->contents = generate_ftree(path);
         			files_traversed++;
+        			
+                // else, not the first file in the directory
         		} else {
-        			struct TreeNode *prev_node = curr_node;
-        			curr_file = readdir(d_ptr);
-        			prev_node->next = generate_ftree(curr_file->d_name);
-        		}
         		
+                    struct TreeNode *prev_node;
+
+                    if (fileSystem->contents != NULL) {
+                    	struct TreeNode *curr_node = fileSystem->contents;
+                    	while (curr_node->next != NULL) {
+                    		curr_node = curr_node->next;
+                    	}
+                    	prev_node = curr_node;
+                    } else { prev_node = NULL; }
+
+        			char path[1024];
+
+        			if (strcmp(fname, ".") == 0) { strcpy(path, curr_file->d_name); }
+
+        			else { 
+        				strcpy(path, fname);
+        				strcat(path, "/");
+        				strcat(path, curr_file->d_name);
+        			} 
+
+        			prev_node->next = generate_ftree(path);
+        		}
         	}
-              
+
         }
 
         // close directory and check for errors while closing
         int error = closedir(d_ptr);
         if (error != 0) {
-        	perror("closedir");
-        	//return 1;
+        	fprintf(stderr, "closedir error");
+        	exit(1);
         }
-
-
     }
 
     return fileSystem;
+
 }
 
 /*
@@ -126,16 +181,20 @@ void print_ftree(struct TreeNode *root) {
 
     // Your implementation here.
 
+
+
     if (root != NULL) {
+
     	// if its a directory
     	if (root->type == 'd') {
     		printf("===== %s (%c%o) =====\n", root->fname, root->type, root->permissions);
+
     		depth++;
     		if (root->contents != NULL) {
-    			depth++;
     			print_ftree(root->contents);
     		}
 
+            depth--;
     		if (root->next != NULL) {
     			print_ftree(root->next);
     		}
@@ -169,7 +228,6 @@ void deallocate_ftree (struct TreeNode *node) {
 
 		} else {
 			if (node->next != NULL) {
-				// deallocate_ftree((node->next)->fname);
 				deallocate_ftree(node->next);
 				free(node);
 			} else {
@@ -180,86 +238,3 @@ void deallocate_ftree (struct TreeNode *node) {
 
 }  
 
-/* 
- * Calls lstat on a single file and prints all of the information that you need to store in a TreeNode
- * 
- */
-
-int printInfo (const char *path) {
-
-    struct stat stat_buf;
-    
-	if (lstat(path, &stat_buf) == -1) {
-	        perror("lstat");        
-	        return 1;    
-    }
-
-    // print file name
-
-
-    // check type and print
-    if (S_ISREG(stat_buf.st_mode)) {
-          printf("%s is a regular file\n", path);    
-    }
-
-    else if (S_ISDIR(stat_buf.st_mode)) {
-          printf("%s is a directory\n", path);    
-    }
-
-    else {
-    	printf("%s is a link\n", path); 
-    }
-
-
-    // extract permissions and print
-    int permissions = stat_buf.st_mode & 0777;
-    printf("the file permissions are %o\n", permissions);
-
-    return 0;
-
-}
-
- /* 
- *  Checks if a file is a directory and, if it is, uses opendir and readdir to print the names of all the files in the directory
- * 
- */
-
-int isDir (const char *path) {
-    
-    struct stat stat_buf;
-    if (lstat(path, &stat_buf) == -1) {
-	        perror("lstat");        
-	        return 1;    
-    }
-
-    // check if the path is a directory
-    if (S_ISDIR(stat_buf.st_mode)) {
-        DIR *d_ptr = opendir(path);
-
-        if (d_ptr == NULL) {
-        	perror("opendir");
-        	return 1;
-        }
-        
-        // stores info about a directory entry
-        struct dirent *entry_ptr;
-
-        // call readdir to get struct dirent, which represents a directory entry
-        entry_ptr = readdir(d_ptr);
-        printf("Directory %s contains:\n", path);    
-        while (entry_ptr != NULL) {
-            printf("%s\n", entry_ptr->d_name);
-            entry_ptr = readdir(d_ptr);    
-        }
-
-        int error = closedir(d_ptr);
-        if (error != 0) {
-        	perror("closedir");
-        	return 1;
-        }
-
-    }
-
-    return 0;
-
-}
