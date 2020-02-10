d9ff3fa592cb175fcf24935f0eb71c16df583554
Cwiek Julia <cwiekjul@teach.cs.toronto.edu>
2020-02-10 14:28:08 -0500

    commiting copy

--- lab6/copy.c
+++ lab6/copy.c
@@ -13,6 +13,12 @@
 
 char *copy(char *dest, const char *src, int capacity) {
 
+    int i;
+    for (i = 0; i < (capacity - 1) && src[i] != '\0'; i++) {
+    	dest[i] = src[i];
+    }
+
+    dest[i] = '\0';
 
     return dest;
 }
