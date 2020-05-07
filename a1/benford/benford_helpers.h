319553d7f2dd34ba3ab79d11382d28a610062fd9
Cwiek Julia <cwiekjul@teach.cs.toronto.edu>
2020-05-06 20:05:55 -0400

    removing

--- a1/benford/benford_helpers.h
+++
@@ -1,5 +0,0 @@
-#ifndef BASE
-#define BASE 10
-#endif
-
-void add_to_tally(int num, int i, int *tally);
--- a1/life2D/life2D_helpers.h
+++
@@ -1,2 +0,0 @@
-void print_state(int *state, int num_rows, int num_cols);
-void update_state(int *state, int num_rows, int num_cols);
